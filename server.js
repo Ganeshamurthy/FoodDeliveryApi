9988c55ebae97b3f715ab4faaa1ffa09b259621b
Ganeshamurthy <jawaharrock666@gmail.com>
2024-05-06 17:39:55 +0530

    Added payment endpoint and empty user cart endpoint

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { foodCart, foodproduct, sequelize, foodUsers } = require("./ORM");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const { UpdateCartService } = require("./updateCartService");
 app.use(express.json());
 app.use(cors());
 const multer = require("multer");
+
+const stripe = require("stripe")(
+  "sk_test_51Mbj8eSAej2N1Wqjun6hYB6mHrPbUaw6N1p078IZ4c2pa1L1QEOtvYDj9bLi5DUUpNZG4HSEvbQSt7BpyCmliSYK008tDDpvG4"
+);
+app.use(express.static("public"));
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
@@ -70,7 +76,7 @@ app.post("/addCart/:u_id/:p_id", async (req, res) => {
   try {
     if (f_product) {
       if (f_user) {
-       debugger
+        debugger;
         let f_Cart = await foodCart.create({
           p_id: product_id,
           u_id: user_id,
@@ -78,8 +84,6 @@ app.post("/addCart/:u_id/:p_id", async (req, res) => {
           quantity: f_product.dataValues.Quantity,
           total_price: f_product.dataValues.Price,
         });
-        console.log("---------------------------------------------------------------");
-        console.log(f_Cart.dataValues);
         res.status(200).json(f_Cart);
       } else {
         throw "User Not found,Please Login";
@@ -99,8 +103,9 @@ app.get("/getCart/:id", async (req, res) => {
     if (res1 == null) {
       throw "your cart is empty";
     } else {
+      let total = await calculatePrice(uid);
       res.status(200);
-      res.json({ Result: res1, status: 200 });
+      res.json({ Result: res1, status: 200, total_price: total });
     }
   } catch (e) {
     res.status(400);
@@ -115,20 +120,35 @@ app.delete("/deleteCart/:id/:u_id", async (req, res) => {
   let u_id = req.params.u_id;
   try {
     await sequelize.sync();
-
+    let total = await calculatePrice(u_id);
     await foodCart.destroy({
       where: {
-        P_id: cart_id,
         u_id: u_id,
+        p_id:cart_id
       },
     });
-    res.json({ message: "item removed frm cart" });
+    
+    res.json({ message: "item removed frm cart", total_price: total });
   } catch (e) {
     console.log(e);
     res.status(400);
     res.json({ erroe: e });
   }
 });
+
+app.get('/truncate/:uid',async(req,res)=>{
+  try{
+    await sequelize.sync();
+    await foodCart.destroy({
+      where: {
+        u_id: req['params']['uid'],
+      },
+    });
+    res.json({"Message":"Sucess"})
+  } catch(e){
+    res.status(400).json({"Error":"Something went wrong"})
+  }
+})
 //update cart
 let service = new UpdateCartService();
 // sample model of endpoint for update "http://localhost:8080/updateCart?p_id=3&qty=6&u_id=3"
@@ -140,7 +160,8 @@ app.post("/updateCart", async (req, res) => {
       query["qty"],
       query["u_id"]
     );
-    res.json({ Message: result });
+    let total = await calculatePrice(query["u_id"]);
+    res.json({ Message: result, total_price: total });
   } catch (error) {
     res.json(error.message);
   }
@@ -151,7 +172,6 @@ app.post("/loginAuth", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await foodUsers.findOne({ where: { email, password } });
-    console.log(user);
     if (!user) {
       throw new Error("User Not available");
     }
@@ -162,7 +182,7 @@ app.post("/loginAuth", async (req, res) => {
     await user.save();
     res.json({ token: user.token, id: user.user_id });
   } catch (error) {
-    res.json({"Error":error.message});
+    res.json({ Error: error.message });
   }
 });
 
@@ -185,7 +205,7 @@ app.post("/createUser", async (req, res) => {
 
 // Logout route with Sequelize
 app.get("/logOut/:id", async (req, res) => {
-  const id = req['params']['id'];
+  const id = req["params"]["id"];
   try {
     const user = await foodUsers.findByPk(id);
     if (!user) {
@@ -197,6 +217,51 @@ app.get("/logOut/:id", async (req, res) => {
   } catch (error) {
     res.json({ message: error });
   }
+});
+
+async function calculatePrice(id) {
+  let result = await foodCart.findAll({
+    where: { u_id: id },
+    attributes: ["total_price"],
+  });
+
+  if (result !== null) {
+    let total = 0;
+
+    result.map((i) => {
+      total += i.dataValues.total_price;
+    });
+    return total;
+  } else {
+    throw new Error("No user is found");
+  }
+}
+
+app.post("/create-checkout-session", async (req, res) => {
+  const { Product } = req["body"];
+  const lineItems = Product.map((product) => 
+    ({
+      price_data: {
+        currency: "INR",
+        product_data: {
+          name: product.p_name,
+          images: [`http://localhost:8080/getImage?id=${product.p_id}`],
+        },
+        unit_amount: product.total_price * 100,
+      },
+      quantity:product.quantity
+    }));
+  console.log(lineItems);
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: `http://localhost:4200/`,
+    cancel_url: `http://localhost:4200/confirmation?success=false`,
+  });
+
+  console.log(session);
+  res.json({ url: session.url });
 });
 
 app.listen(8080, () => {
